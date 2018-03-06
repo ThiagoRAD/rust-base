7c6081485c570cc88e1e0934eb69450e615f84c1
ThiagoRAD <thauroo@gmail.com>
2018-03-06 15:08:09 -0300

    init

--- camping-app/src/main.rs
+++ camping-app/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+  let mut destination = "Crater Lake";
+  println!("{} is the deepest lake in the US", destination);
+  destination = "Sparks Lake";
+  println!("{} is also a lake in the US", destination);
 }
