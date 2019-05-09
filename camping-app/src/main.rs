ec07c24c653f139dd8857ba99b5e5edddbad1ae4
ThiagoRAD <thauroo@gmail.com>
2019-05-09 15:09:15 -0300

    const

--- camping-app/src/main.rs
+++ camping-app/src/main.rs
@@ -1,6 +1,4 @@
 fn main() {
-  let mut destination = "Crater Lake";
-  println!("{} is the deepest lake in the US", destination);
-  destination = "Sparks Lake";
-  println!("{} is also a lake in the US", destination);
+  const DESTINATION: &str = "Crater Lake";
+  println!("{} is the deepest lake in the US", DESTINATION);
 }
