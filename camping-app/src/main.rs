0775a97ac0b8cc03be2d579c89635d5b69d2d305
ThiagoRAD <thauroo@gmail.com>
2019-09-08 14:24:42 -0300

    meals

--- camping-app/src/main.rs
+++ camping-app/src/main.rs
@@ -4,4 +4,8 @@ fn main() {
 
   let backpack_weight: f32 = 28.5;
   println!("hiking for {} days with a {} lb. pack", days, backpack_weight);
+
+  let meals = days * 3;
+  println!("{} meals", meals);
+
 }
