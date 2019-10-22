7c30c3210fb572bc028221372732e62ae85d0e65
ThiagoRAD <thauroo@gmail.com>
2019-10-22 13:21:48 -0300

    update

--- camping-app/src/main.rs
+++ camping-app/src/main.rs
@@ -1,17 +1,25 @@
-struct Hiker {
-  name: String, 
-  miles_hiked: u32,
-}
+use std::fs;
 
 fn main() {
-  let jennifer = Hiker {
-    name: String::from("Jennifer"),
-    miles_hiked: 49,
-  };
-  println!("{} has hiked {} miles", jennifer.name, jennifer.miles_hiked);
-  let bob = Hiker {
-    name: String::from("Bob"),
-    miles_hiked: 15,
-  };
-  println!("{} has hiked {} miles", bob.name, bob.miles_hiked);
+  let text = fs::read_to_string("./my_file.txt").expect("Something went wrong reading the file");
+  println!("What is in this file:\n{}", text);
 }
+
+
+// struct Hiker {
+//   name: String, 
+//   miles_hiked: u32,
+// }
+
+// fn main() {
+//   let jennifer = Hiker {
+//     name: String::from("Jennifer"),
+//     miles_hiked: 49,
+//   };
+//   println!("{} has hiked {} miles", jennifer.name, jennifer.miles_hiked);
+//   let bob = Hiker {
+//     name: String::from("Bob"),
+//     miles_hiked: 15,
+//   };
+//   println!("{} has hiked {} miles", bob.name, bob.miles_hiked);
+// }
