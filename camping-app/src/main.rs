a31a20f01b57de65ab99674916dde0eec11cca3f
ThiagoRAD <thauroo@gmail.com>
2021-03-09 16:17:04 -0300

    lists

--- camping-app/src/main.rs
+++ camping-app/src/main.rs
@@ -1,14 +1,4 @@
-fn calculate_distance(days: u64, distance: u64) -> Result<u64, String> {
-  if days == 0 {
-    return Err("Cannot go on a zero day hike".to_string());
-  }
-  return Ok(days * distance);
-}
-
 fn main() {
-  let result = calculate_distance(0, 10);
-  match result {
-      Ok(miles) => println!("Miles: {}", miles),
-      Err(error) => println!("Error: {}", error),
-  }
+  let years: [i32; 3] = [2020, 2021, 2022];
+  println!("Years: {:?}", years);
 }
