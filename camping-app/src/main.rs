35a0c074674d79499f9a89eec19986dda5a0d52e
ThiagoRAD <thauroo@gmail.com>
2021-06-29 15:14:41 -0300

    booleans

--- camping-app/src/main.rs
+++ camping-app/src/main.rs
@@ -1,9 +1,6 @@
-/*
-In this file we set a destination!
-*/
-
-/// This function is used throughout the entire project to state where the destionation is
 fn main() {
-  const DESTINATION: &str = "Crater Lake";
-  println!("{} is the deepest lake in the US", DESTINATION);
+  let summer = true;
+  let winter = true;
+  println!("Summer is the best season: {}", summer);
+  println!("Winter is also good: {}", winter);
 }
