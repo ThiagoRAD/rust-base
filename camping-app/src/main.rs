c1492fc1e3af6af2d430c528094aadf5ac43c19e
ThiagoRAD <thauroo@gmail.com>
2022-12-30 14:39:35 -0300

    treating errors

--- camping-app/src/main.rs
+++ camping-app/src/main.rs
@@ -1,9 +1,15 @@
-fn calculate_distance(days: u64, distance: u64) -> u64 {
+fn calculate_distance(days: u64, distance: u64) -> Result<u64, String> {
+  if days == 0 {
+    return Err(String::from("Days cannot be zero"));
+  }
   let total_miles = days * distance;
-  return total_miles;
+  return Ok(total_miles);
 }
 
 fn main() {
   let result = calculate_distance(5, 10);
-  println!("{} miles", result);
+  match result {
+      Ok(miles) => println!("{} miles", miles),
+      Err(e) => println!("Error: {}", e),
+  }
 }
