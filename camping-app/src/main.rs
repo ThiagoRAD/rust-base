92bcc8df5b5de954ead9c27de38beae652b969c8
ThiagoRAD <thauroo@gmail.com>
2024-01-23 14:38:21 -0300

    removing comma

--- camping-app/src/main.rs
+++ camping-app/src/main.rs
@@ -5,5 +5,5 @@ fn calculate_distance(days: u64, distance: u64) -> u64 {
 
 fn main() {
   let result = calculate_distance(5, 10);
-  println!("{}, miles", result);
+  println!("{} miles", result);
 }
