787a49c1d671de28f7f4e25cfee24dfb55650268
ThiagoRAD <thauroo@gmail.com>
2024-07-29 15:11:18 -0300

    comments

--- camping-app/src/main.rs
+++ camping-app/src/main.rs
@@ -1,3 +1,8 @@
+/*
+In this file we set a destination!
+*/
+
+/// This function is used throughout the entire project to state where the destionation is
 fn main() {
   const DESTINATION: &str = "Crater Lake";
   println!("{} is the deepest lake in the US", DESTINATION);
