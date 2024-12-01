1a4556c7233aab23c1a7628ae8ca034f017a49c5
ThiagoRAD <thauroo@gmail.com>
2024-12-01 15:16:25 -0300

    booleans and else if

--- camping-app/src/main.rs
+++ camping-app/src/main.rs
@@ -3,4 +3,12 @@ fn main() {
   let winter = true;
   println!("Summer is the best season: {}", summer);
   println!("Winter is also good: {}", winter);
+
+  if summer {
+    println!("go biking!");
+  } else if winter {
+    println!("go skiing!");
+  } else {
+    println!("do something else!");
+  }
 }
