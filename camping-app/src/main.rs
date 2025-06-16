950464986290751e010490596c8421ff42877d06
ThiagoRAD <thauroo@gmail.com>
2025-06-16 08:59:17 -0300

    struct

--- camping-app/src/main.rs
+++ camping-app/src/main.rs
@@ -1,15 +1,17 @@
-#[derive(Debug)]
-enum Steepness {
-  Easy, 
-  Moderate, 
-  Difficult,
+struct Hiker {
+  name: String, 
+  miles_hiked: u32,
 }
 
 fn main() {
-  let calm_trail = Steepness::Easy;
-  println!("Steepness is {:?}", calm_trail);
-  let fun_trail = Steepness::Moderate;
-  println!("Steepness is {:?}", fun_trail);
-  let pricly_peak_trail = Steepness::Difficult;
-  println!("Steepness is {:?}", pricly_peak_trail);
+  let jennifer = Hiker {
+    name: String::from("Jennifer"),
+    miles_hiked: 49,
+  };
+  println!("{} has hiked {} miles", jennifer.name, jennifer.miles_hiked);
+  let bob = Hiker {
+    name: String::from("Bob"),
+    miles_hiked: 15,
+  };
+  println!("{} has hiked {} miles", bob.name, bob.miles_hiked);
 }
